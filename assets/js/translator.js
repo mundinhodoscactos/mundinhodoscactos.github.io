b8455ee6372ed9b310b48420c83b18814a7bacbc
mundinhodoscactos <90579033+mundinhodoscactos@users.noreply.github.com>
2021-09-12 19:14:53 -0300

    Update translator.js
    edit

--- assets/js/translator.js
+++ assets/js/translator.js
@@ -1,37 +1,37 @@
 (function(){
     const PortuguêsInput = document.getElementById('PortuguêsInput');
-    const cactobetoInput = document.getElementById('CactobetoInput');
+    const CactobetoInput = document.getElementById('CactobetoInput');
     const input = document.getElementsByClassName('input');
     const output = document.getElementsByClassName('output');
     const translate = document.getElementById('translate');
     const revert = document.getElementById('revert');
     const alphabet = {
-        'a': 'i', 'A': 'I', 
-        'b': 'c', 'B': 'C', 
-        'c': 't', 'C': 'T', 
-        'd': 'f', 'D': 'F', 
-        'e': 'o', 'E': 'O', 
+        'a': 'y', 'A': 'Y', 
+        'b': 'p', 'B': 'P', 
+        'c': 'l', 'C': 'L', 
+        'd': 't', 'D': 'T', 
+        'e': 'a', 'E': 'A', 
         'f': 'v', 'F': 'V', 
-        'g': 's', 'G': 'S', 
-        'h': 'd', 'H': 'D', 
-        'i': 'a', 'I': 'A', 
-        'j': 'e', 'J': 'E', 
+        'g': 'k', 'G': 'K', 
+        'h': 'r', 'H': 'R', 
+        'i': 'e', 'I': 'E', 
+        'j': 'z', 'J': 'Z', 
         'k': 'g', 'K': 'G', 
-        'l': 'j', 'L': 'J', 
-        'm': 'l', 'M': 'L', 
-        'n': 'y', 'N': 'Y', 
-        'o': 'b', 'O': 'B', 
-        'p': 'h', 'P': 'H', 
-        'q': 'm', 'Q': 'M', 
+        'l': 'm', 'L': 'M', 
+        'm': 's', 'M': 'S', 
+        'n': 'h', 'N': 'H', 
+        'o': 'u', 'O': 'U', 
+        'p': 'b', 'P': 'B', 
+        'q': 'x', 'Q': 'X', 
         'r': 'n', 'R': 'N', 
-        's': 'p', 'S': 'P', 
-        't': 'q', 'T': 'Q', 
-        'u': 'r', 'U': 'R', 
-        'v': 'u', 'V': 'U', 
-        'x': 'w', 'X': 'W', 
-        'w': 'k', 'W': 'K', 
-        'y': 'z', 'Y': 'Z', 
-        'z': 'x', 'Z': 'X', 
+        's': 'c', 'S': 'C', 
+        't': 'd', 'T': 'D', 
+        'u': 'i', 'U': 'I', 
+        'v': 'j', 'V': 'J', 
+        'x': 'q', 'X': 'Q', 
+        'w': 'f', 'W': 'F', 
+        'y': 'o', 'Y': 'O', 
+        'z': 'w', 'Z': 'W', 
     }
     const alphabetCactobeto = new Object;
     for (var letter in alphabet) {
